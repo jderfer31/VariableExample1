0b783ef8e1f531b7a6de52608b9b9c7ec8bc2269
jderfer0979@gmail.com <jderfer0979@gmail.com>
2020-06-01 18:05:18 -0400

    Add project files.

---
+++ VariableExample1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Reflection.PortableExecutable;
+
+namespace VariableExample1
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            //string and int are varialbles you can use to make simple edits to large pieces of code - I olnly have to change name and age
+            string characterName = "Jeremy";
+            int characterAge;
+            characterAge = 25;
+            Console.WriteLine("There once was a man named " + characterName);
+            Console.WriteLine("He was " + characterAge + " years old");
+
+            //You can also change the variable half-way throug by using below
+            characterName = "Mike";
+            Console.WriteLine("He really liked the name " + characterName);
+            Console.WriteLine("But didn't like being " + characterAge);
+
+            
+        }
+    }
+}
